4a02c3c260359bb07f1470ce53de4ebb06bf7a52
Jack Schedel <jackschedel@gmail.com>
2022-04-18 12:53:16 -0400

    initialized project

---
+++ cmake-build-debug/CMakeFiles/EndGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EndGame.dir/main.cpp.obj"
+  "EndGame.exe"
+  "EndGame.exe.manifest"
+  "EndGame.pdb"
+  "libEndGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EndGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
